4ab0dbd929746bfa3249001a6611ddd8107089d1
lwaaron2023 <alwaller@wpi.edu>
2025-10-04 20:29:49 -0400

    Fixed small bugs

--- src/App.jsx
+++ src/App.jsx
@@ -31,7 +31,7 @@ function App() {
     }
 
     const initializeConnection = () =>{
-        setWs([new WebSocket( 'ws://127.0.0.1:3000' )])
+        setWs([new WebSocket( 'ws://127.0.0.1:3000/ws' )])
     }
 
     useEffect(() => {
@@ -191,7 +191,7 @@ function App() {
                 <Placegrid id={placeGridId} cols={cols} rows={rows} size={size} cb={setPlayMode} extractInfo={extractInfo}/>
             </div>
         </div>
-        <div className = "hidden min-h-fit min-w-fit m-4 p-4 border-2 rounded-xl bg-gray-300 flex-cols justify-center align-center" id={"post-game"} style={{"width":"50%","justify-self":"center","margin-top":"5rem"}}>
+        <div className = "hidden min-h-fit min-w-fit m-4 p-4 border-2 rounded-xl bg-gray-300 flex-cols justify-center align-center" id={"post-game"} style={{"width":"50%","justifySelf":"center","marginTop":"5rem"}}>
             <p className={"p-2 text-center  text-4xl mb-2"} id={"post-game-message"}></p>
             <button type={"button"} className={"text-center text-4xl text-blue-200 hover:text-blue-500 p-4 border-2 border-black rounded-xl bg-neutral-700"} style={{"width":"100%"}} onClick={()=>{window.location.reload()}}>Play Again?</button>
         </div>
